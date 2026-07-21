5d2a3d0ca80ac696bc39a941d916c674bcff794e
fustarc <fustarc@naver.com>
2026-07-21 19:12:51 +0900

    Buyer.java 업데이트

--- classmakingexam/Buyer.java
+++ classmakingexam/Buyer.java
@@ -13,10 +13,35 @@ public class Buyer {
 	}
 	
 	public void buy(Seller seller, int requestCount) {
-		int actualCount = requestCount;
-		if (actualCount > seller.getStock()) {
-			actualCount = seller.getStock();
+		int purchaseCount = requestCount;
+		if (purchaseCount > seller.getStock()) {
+			purchaseCount = seller.getStock();
 		}
 		
+  if (purchaseCount <= 0) {
+   System.out.println("구매실패: 판매 재고 부족");
+   return;
+  }
+
+
+   int totalPrice = purchaseCount * seller.getPrice();
+
+   if (money < totalPrice) {
+    System.out.println("구매실패: 보유 현금 부족");
+    return;
+   }
+
+   seller.processSales(purchaseCount);
+   money -= totalPrice;
+   basket += purchaseCount;
+
+   System.out.println("구매성공: " + purchaseCount + "개 구매");
+
+   }
+
+   
+
+
+
 	}
 }
