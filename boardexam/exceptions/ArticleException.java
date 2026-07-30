be8acf8d5240ccf2594e4f0a9be0d49f803ce68c
fustarc <fustarc@naver.com>
2026-07-30 17:38:21 +0900

    Add files via upload

--- boardexam/exceptions/ArticleException.java
+++ boardexam/exceptions/ArticleException.java
@@ -1,5 +1,14 @@
 package com.ktdsuniversity.edu.oop.exam.boardexam.exceptions;
 
-public class ArticleException {
+public class ArticleException extends RuntimeException {
+
+	/**
+	 * 게시글 제목을 입력하지 않았거나, 길이가 초과 할때 발생하는 예외입니다.
+	 */
+	private static final long serialVersionUID = 5801752971992873705L;
+	
+	public ArticleException (String message) {
+		super(message);
+	}
 
 }
--- boardexam/exceptions/ArticleWriterException.java
+++ boardexam/exceptions/ArticleWriterException.java
@@ -1,5 +1,14 @@
 package com.ktdsuniversity.edu.oop.exam.boardexam.exceptions;
 
-public class ArticleWriterException {
+public class ArticleWriterException extends RuntimeException {
+
+	/**
+	 * 게시글 작성자의 이름이 입력되지 않았을 때 발생하는 예외입니다.
+	 */
+	private static final long serialVersionUID = -6096248616964014409L;
+	
+	public ArticleWriterException(String message) {
+		super(message);
+	}
 
 }
