fdb00594b2bf56a1150e626bc51e669df08f0d3e
fustarc <fustarc@naver.com>
2026-08-03 08:52:47 +0900

    Delete boardexam/CommentServImpl.java

--- boardexam/CommentServImpl.java
+++
@@ -1,7 +0,0 @@
-package com.ktdsuniversity.edu.oop.exam.boardexam;
-
-import com.ktdsuniversity.edu.oop.exam.boardexam.inf.CommentService;
-
-public class CommentServImpl implements CommentService {
-
-}
