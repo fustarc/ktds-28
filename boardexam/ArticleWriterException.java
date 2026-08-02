84a34748bcdc4807badcb2d53dafa5fe3e39c4c6
fustarc <fustarc@naver.com>
2026-08-02 22:57:42 +0900

    ArticleWriterException.java 업데이트

--- boardexam/ArticleWriterException.java
+++ boardexam/ArticleWriterException.java
@@ -1,5 +1,7 @@
 package com.ktdsuniversity.edu.oop.exam.boardexam;
 
-public class ArticleWriterException {
-
+public class ArticleWriterException extends RuntimeException {
+    public ArticleWriterException(String message) {
+        super(message);
+    }
 }
