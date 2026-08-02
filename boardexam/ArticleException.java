b92d193483eae954fd2fcc2c1c1dd827bf885661
fustarc <fustarc@naver.com>
2026-08-02 23:18:42 +0900

    ArticleException.java 삭제

--- boardexam/ArticleException.java
+++
@@ -1,7 +0,0 @@
-package com.ktdsuniversity.edu.oop.exam.boardexam;
-
-public class ArticleException extends RuntimeException {
-    public ArticleException(String message) {
-        super(message);
-    }
-}
