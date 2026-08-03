07e3e31844497c4a2fb0d774a04c7bafa9304cc8
fustarc <fustarc@naver.com>
2026-08-03 14:51:13 +0900

    Update comments in CommentService interface

--- boardexam/inf/CommentService.java
+++ boardexam/inf/CommentService.java
@@ -6,7 +6,7 @@ package com.ktdsuniversity.edu.oop.exam.boardexam.inf;
 public interface CommentService {
 
     /**
-     * 1. 지정한 게시글에 댓글을 등록합니다.
+     * 지정한 게시글에 댓글을 등록합니다.
      * 게시글 하나당 최대 10개의 댓글만 등록할 수 있습니다.
      *
      * @param articleId 댓글을 등록할 게시글 번호
@@ -17,7 +17,7 @@ public interface CommentService {
     void createComment(int articleId, String content, String writer, String regDate);
 
     /**
-     * 2. 지정한 게시글의 댓글을 삭제합니다.
+     * 지정한 게시글의 댓글을 삭제합니다.
      *
      * @param articleId 댓글이 등록된 게시글 번호
      * @param commentId 삭제할 댓글 번호
@@ -25,7 +25,7 @@ public interface CommentService {
     void deleteComment(int articleId, int commentId);
 
     /**
-     * 3. 지정한 게시글의 댓글을 추천합니다.
+     * 지정한 게시글의 댓글을 추천합니다.
      *
      * @param articleId 댓글이 등록된 게시글 번호
      * @param commentId 추천할 댓글 번호
@@ -33,7 +33,7 @@ public interface CommentService {
     void recommendComment(int articleId, int commentId);
 
     /**
-     * 4. 지정한 게시글에 등록된 모든 댓글을 삭제합니다.
+     * 지정한 게시글에 등록된 모든 댓글을 삭제합니다.
      *
      * @param articleId 댓글을 모두 삭제할 게시글 번호
      */
