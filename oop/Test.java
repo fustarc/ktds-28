4eb080c57b8587052bb4436af8f47acd32565deb
fustarc <fustarc@naver.com>
2026-08-04 17:01:03 +0900

    Add files via upload

---
+++ oop/Test.java
@@ -0,0 +1,23 @@
+package com.ktdsuniversity.edu.oop;
+
+import com.ktdsuniversity.edu.oop.exam.Car;
+import com.ktdsuniversity.edu.oop.exam.Student;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Student student = new Student(0,0,0,0);
+		
+		String name = "ASDFG";
+		double rnd = Math.random();
+		
+		Car testCar = new Car(10);
+		testCar.pressEngineStartButton();
+		System.out.println(testCar.zero100Sec);
+		
+		Scanner keyboard = new Scanner(System.in);
+	}
+}
