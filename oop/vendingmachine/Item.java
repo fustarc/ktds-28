cfc95f06b7c717f605df36d50d9a43c76777ee66
fustarc <fustarc@naver.com>
2026-08-04 17:03:14 +0900

    Add files via upload

---
+++ oop/vendingmachine/Item.java
@@ -0,0 +1,34 @@
+package com.ktdsuniversity.edu.oop.vendingmachine;
+
+/**
+ * 자판기에서 판매하는 상품의 정보
+ */
+public class Item {
+
+	/**
+	 * 상품 번호
+	 */
+	String itemId;
+	
+	/**
+	 * 상품명
+	 */
+	String itemName;
+	
+	/**
+	 * 상품 가격
+	 */
+	int itemPrice;
+	
+	/**
+	 * 상품 재고
+	 */
+	int itemStock;
+	
+	public Item(String itemID, String itemName, int itemPrice, int itemStock) {
+		this.itemId = itemID;
+		this.itemName = itemName;
+		this.itemPrice = itemPrice;
+		this.itemStock = itemStock;
+	}
+}
